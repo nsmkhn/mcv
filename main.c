10ff43eb0d3a43e7eeb02064a39b49981958432f
Nikita Tokariev <mykyta.tokariev@gmail.com>
2021-08-15 13:00:26 +0300

    got rid of reduntant colors[]

--- main.c
+++ main.c
@@ -24,16 +24,14 @@ typedef struct
 } RGB;
 
 void
-reset_pixels(uint32_t *pixels, size_t num_pixels, uint32_t color)
+fill_pixels(RGB *pixels, size_t num_pixels, RGB color)
 {
     for(size_t i = 0; i < num_pixels; ++i)
         pixels[i] = color;
 }
 
 void
-save_as_ppm(char *filepath,
-            uint32_t *pixels, size_t width, size_t height,
-            RGB *colors)
+save_as_ppm(char *filepath, RGB *pixels, size_t width, size_t height)
 {
     FILE *file;
     if((file = fopen(filepath, "w")) == NULL)
@@ -42,23 +40,9 @@ save_as_ppm(char *filepath,
         exit(EXIT_FAILURE);
     }
 
-    RGB *pcolors = colors;
     fprintf(file, "P6\n%zu %zu 255\n", width, height);
-    for(size_t y = 0; y < height; ++y)
-    {
-        for(size_t x = 0; x < width; ++x)
-        {
-            uint32_t pixel = pixels[y * width + x];
-            *pcolors++ = (RGB)
-            {
-                .r = (pixel >> 8 * 2) & 0xFF,
-                .g = (pixel >> 8 * 1) & 0xFF,
-                .b = (pixel >> 8 * 0) & 0xFF
-            };
-        }
-    }
+    fwrite(pixels, sizeof(RGB), width*height, file);
 
-    fwrite(colors, sizeof(RGB), width*height, file);
     if(fclose(file) == EOF)
     {
         fprintf(stderr, "Failed to close file %s: %s", filepath, strerror(errno));
@@ -68,12 +52,12 @@ save_as_ppm(char *filepath,
 }
 
 void
-stripes_pattern(uint32_t *pixels,
+stripes_pattern(RGB *pixels,
                 size_t width,
                 size_t height,
                 size_t tile_size,
-                uint32_t background,
-                uint32_t foreground)
+                RGB background,
+                RGB foreground)
 {
     for(size_t y = 0; y < height; ++y)
     {
@@ -86,12 +70,12 @@ stripes_pattern(uint32_t *pixels,
 }
 
 void
-checker_pattern(uint32_t *pixels,
+checker_pattern(RGB *pixels,
                 size_t width,
                 size_t height,
                 size_t tile_size,
-                uint32_t background,
-                uint32_t foreground)
+                RGB background,
+                RGB foreground)
 {
     for(size_t y = 0; y < height; ++y)
     {
@@ -104,12 +88,12 @@ checker_pattern(uint32_t *pixels,
 }
 
 void
-solid_circle_pattern(uint32_t *pixels,
-                    size_t width,
-                    size_t height,
-                    size_t radius,
-                    uint32_t background,
-                    uint32_t foreground)
+solid_circle_pattern(RGB *pixels,
+                     size_t width,
+                     size_t height,
+                     size_t radius,
+                     RGB background,
+                     RGB foreground)
 {
     int64_t cx = width;
     int64_t cy = height;
@@ -127,11 +111,11 @@ solid_circle_pattern(uint32_t *pixels,
 }
 
 void
-hollow_circle_pattern(uint32_t *pixels,
+hollow_circle_pattern(RGB *pixels,
                       size_t width,
                       size_t height,
                       size_t radius,
-                      uint32_t color)
+                      RGB color)
 {
     assert(width == height);
     float w = width, h = height;
@@ -164,23 +148,25 @@ hollow_circle_pattern(uint32_t *pixels,
 int
 main()
 {
-    static uint32_t pixels[NUM_PIXELS];
-    static RGB colors[NUM_PIXELS];
+    static RGB pixels[NUM_PIXELS];
+    RGB honeydew = *(RGB *) &(int){HONEYDEW};
+    RGB indigo = *(RGB *) &(int){INDIGO};
+    RGB black = *(RGB *) &(int){BLACK};
 
-    stripes_pattern(pixels, WIDTH, HEIGHT, TILE_SIZE, HONEYDEW, INDIGO);
-    save_as_ppm("stripes.ppm", pixels, WIDTH, HEIGHT, colors);
+    stripes_pattern(pixels, WIDTH, HEIGHT, TILE_SIZE, honeydew, indigo);
+    save_as_ppm("stripes.ppm", pixels, WIDTH, HEIGHT);
 
-    reset_pixels(pixels, WIDTH*HEIGHT, BLACK);
-    checker_pattern(pixels, WIDTH, HEIGHT, TILE_SIZE, HONEYDEW, INDIGO);
-    save_as_ppm("checker.ppm", pixels, WIDTH, HEIGHT, colors);
+    fill_pixels(pixels, WIDTH*HEIGHT, black);
+    checker_pattern(pixels, WIDTH, HEIGHT, TILE_SIZE, honeydew, indigo);
+    save_as_ppm("checker.ppm", pixels, WIDTH, HEIGHT);
 
-    reset_pixels(pixels, WIDTH*HEIGHT, BLACK);
-    solid_circle_pattern(pixels, WIDTH, HEIGHT, RADIUS, HONEYDEW, INDIGO);
-    save_as_ppm("solid_circle.ppm", pixels, WIDTH, HEIGHT, colors);
+    fill_pixels(pixels, WIDTH*HEIGHT, black);
+    solid_circle_pattern(pixels, WIDTH, HEIGHT, RADIUS, honeydew, indigo);
+    save_as_ppm("solid_circle.ppm", pixels, WIDTH, HEIGHT);
 
-    reset_pixels(pixels, WIDTH*HEIGHT, HONEYDEW);
-    hollow_circle_pattern(pixels, WIDTH, HEIGHT, RADIUS, INDIGO);
-    save_as_ppm("hollow_circle.ppm", pixels, WIDTH, HEIGHT, colors);
+    fill_pixels(pixels, WIDTH*HEIGHT, honeydew);
+    hollow_circle_pattern(pixels, WIDTH, HEIGHT, RADIUS, indigo);
+    save_as_ppm("hollow_circle.ppm", pixels, WIDTH, HEIGHT);
 
     return 0;
 }
